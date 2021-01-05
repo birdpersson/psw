7b56b6b2063aed16b61cb65de7d3a895dfb19b32
Aleksandar Vujasin <aleksandarvujasin@gmail.com>
2021-01-05 22:01:33 +0100

    Initial database

---
+++ HospitalWebApp/HospitalWebApp/Models/MyDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalWebApp.Models
+{
+    public class MyDbContext: DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+    }
+}
